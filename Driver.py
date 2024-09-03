19c172e4e1003faefe0b938fbbc9e25653dedbe1
Josh Rigney <rigneyj106@gmail.com>
2024-09-03 16:35:26 -0400

    Add packages, make requests, and test output

--- Driver.py
+++ Driver.py
@@ -1,2 +1,14 @@
-msg = "Hello World"
-print(msg)
+# A program for scraping webpages for images and saving them to the file system
+# Import packages
+from bs4 import BeautifulSoup
+import requests
+
+# Make a get request to retrieve webpage and pass through BeautifulSoup for parsing
+html_page = requests.get('http://books.toscrape.com')
+soup = BeautifulSoup(html_page.content, 'html.parser')
+warning = soup.find('div', class_="alert alert-warning")
+book_container = warning.nextSibling.nextSibling
+
+images = book_container.findAll('img')
+example = images[0]
+example
